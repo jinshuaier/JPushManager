d2c89ff8874109dac08b73845deb859a912359d6
jinshuaier <470401910@qq.com>
2017-11-13 17:44:07 +0800

    first commit

---
+++ JPushManager/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  JPushManager
+//
+//  Created by Doman on 17/3/31.
+//  Copyright © 2017年 doman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+static NSString *appKey = @"你的appKey";
+static NSString *channel = @"APP Store";
+static BOOL isProduction = YES;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
